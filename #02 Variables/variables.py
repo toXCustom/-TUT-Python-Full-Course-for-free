0cb2db8bcbb58b412754eadeefa19b840bd43965
toXCustom <pawel89@gmail.com>
2024-05-15 17:30:42 +0200

    00h 10m 05s

---
+++ #02 Variables/variables.py
@@ -0,0 +1,7 @@
+# variable = a container for a value. Behaves as the value that it contains.
+
+first_name = "Pawel"
+last_name = "Mroz"
+full_name = first_name + " " + last_name
+
+print("Hello " + full_name)
