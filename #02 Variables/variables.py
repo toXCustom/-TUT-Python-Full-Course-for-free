b6f8d3668700f11194a69f25a1bf28de70f4b476
toXCustom <pawel89@gmail.com>
2024-05-21 10:07:03 +0200

    01h30m14s

--- #02 Variables/variables.py
+++ #02 Variables/variables.py
@@ -1,7 +1,21 @@
 # variable = a container for a value. Behaves as the value that it contains.
-
 first_name = "Pawel"
 last_name = "Mroz"
 full_name = first_name + " " + last_name
 
 print("Hello " + full_name)
+
+# int
+age = 21
+age += 1
+print("You age is: " + str(age))
+
+# float - floating point number (a decimal number)
+height = 250.5
+print("You height is: " + str(height) + "cm")
+print(type(height))
+
+# boolean - True or False
+human = True
+print("Are you a human: " + str(human))
+print(type(human))
---
+++ #05 Type cast/type_cast.py
@@ -0,0 +1,19 @@
+#type casting = convert the data type of a value to another data type.
+
+x = 1   #int
+y = 2.0 #float
+z = "3" #string
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+print(x)
+print(y)
+print(z*3)
+
+x = str(x)
+y = str(y)
+
+print("X is " + x)
+print("Y is " + y)
